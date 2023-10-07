0dcad2d0526da6245f428d8c46caf6c305126fce
Zach Myre <myrezach@live.com>
2023-10-07 12:00:40 -0500

    clap installed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 // CLI tool for building, tagging, and pushing docker containers.
 
+// example) rdock ../.dockerfile
+// where argument is only path, rdock finds the first build, tag, and push tags to use
+
+// example) rdock -c sierra-api ../../.dockerfile
+// where -c says find the comment where the next arg is found for each build, tag, and push. then last is the path to docker file.
+// good for projects with multiple builds in same file
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
